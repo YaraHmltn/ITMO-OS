1d4aef714c63eadb12554c7cb2efc529a469fc08
YaraHmltn <yaroslavavdovina@icloud.com>
2020-10-02 17:49:11 +0300

    mmm 4:50

---
+++ Lab_1/log.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+log ()
+{
+Warning = $(cat /var/log/anaconda/X.log | awk '/\[ && /WW/')
+Information= $(cat /var/log/anaconda/X.log | awk '/\[/ && /II/')
+echo -e "${Warning//"(WW)"/" \e[1;33mWarning:\e[0m"}"
+echo -e "${Infornation//"(WW)"/"\e[1;36mInformation:\e[0m"}"
+}
