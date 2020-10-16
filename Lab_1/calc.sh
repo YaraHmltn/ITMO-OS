d205b7d7adcee5b5a39060447c042975262b05b6
YaraHmltn <yaroslavavdovina@icloud.com>
2020-10-16 14:04:52 +0300

    fix

--- Lab_1/calc.sh
+++ Lab_1/calc.sh
@@ -6,7 +6,7 @@ if [[ "$1" -ne "sum" ]] && [[ "$1" -ne "sub" ]] && [[ "$1" -ne "mul" ]] && [["$1
 then echo "First argument is invalid"
 exit 2
 fi
-if [[ "$1" -eq "div" && ([[ "$3" -eq "0" ]] || [[ "$3" -eq "-0" ]] || [[ "$3" -eq "+0" ]])
+if [[ "$1" -eq "div" ]] && ( [[ "$3" -eq "0" ]] || [[ "$3" -eq "-0" ]] || [[ "$3" -eq "+0" ]] )
 then echo "Cannot div by zero"
 exit 3
 fi
