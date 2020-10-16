559fb94283415a3c422aa437a6478783d52f49d7
YaraHmltn <yaroslavavdovina@icloud.com>
2020-10-16 12:30:01 +0300

    blinblyat'

--- Lab_1/log.sh
+++ Lab_1/log.sh
@@ -5,5 +5,5 @@ log ()
 Warning = $(cat /var/log/anaconda/X.log | awk '/\[ && /WW/')
 Information= $(cat /var/log/anaconda/X.log | awk '/\[/ && /II/')
 echo -e "${Warning//"(WW)"/" \e[1;33mWarning:\e[0m"}"
-echo -e "${Infornation//"(WW)"/"\e[1;36mInformation:\e[0m"}"
+echo -e "${Infornation//"(II)"/"\e[1;36mInformation:\e[0m"}"
 }
